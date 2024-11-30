ef1f997431543350e07a13e0de036c83538b5f2a
ari <ariadnerv17@gmail.com>
2024-11-30 00:46:52 -0300

    Continuação do Projeto Screenmatch

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -3,8 +3,8 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosSerie(@JsonAlias("title") String titulo,
+@JsonIgnoreProperties(ignoreUnknown = true) //ignora os campos que faltam do json aqui na classe
+public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
 }
--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -1,4 +1,5 @@
 package br.com.alura.screenmatch.service;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados {
@@ -6,7 +7,10 @@ public class ConverteDados implements IConverteDados {
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
-        return mapper.convertValue(json, classe);
-
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
